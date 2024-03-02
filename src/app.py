2b9a00af837a797e41667d4861cda6f3f4c035e8
dgasensi <dgasensi@gmail.com>
2024-03-02 13:42:42 +0100

    commit final de la practica sobre apis

--- src/app.py
+++ src/app.py
@@ -1,15 +1,34 @@
 import os
 from sqlalchemy import create_engine
 import pandas as pd
+import spotipy
 from dotenv import load_dotenv
-
-# load the .env file variables
+from spotipy.oauth2 import SpotifyClientCredentials
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+spot = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id = client_id,
+                                                              client_secret = client_secret))
+SW_uri = 'spotify:artist:7guDJrEfX3qb6FEbdPA5qi'
+
+results = spot.artist_top_tracks(SW_uri)
+
+top10_name= []
+top10_duration_ms = []
+top10_popularity = []
+for track in results['tracks'][:10]:
+    top10_name.append(track['name'])
+    top10_duration_ms.append(round((track['duration_ms']/1000)/60, 2))
+    top10_popularity.append(track['popularity'])
+    
+print(top10_duration_ms)
+top10_df = pd.DataFrame(list(zip(top10_name, top10_popularity, top10_duration_ms)), columns=['nombre', 'popularidad', 'duracion_minutos'])
+print(top10_df)
+
+import seaborn as sbn
+
+sbn.scatterplot(x='popularidad', y='duracion_minutos', data=top10_df)
